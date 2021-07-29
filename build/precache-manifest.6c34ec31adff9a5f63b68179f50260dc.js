99662807e1b943556a3e874bddcbe9073069b169
Juniper <juneconsolidated@gmail.com>
2021-07-29 09:19:12 -0700

    Heroku deployment

---
+++ build/precache-manifest.6c34ec31adff9a5f63b68179f50260dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5ef8e67fc946ca5b3652ac61c7fad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfbceaeeb08b91e79fd7",
+    "url": "/static/css/main.6d7020f5.chunk.css"
+  },
+  {
+    "revision": "f098a9f642545862f988",
+    "url": "/static/js/2.73817f5d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.73817f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbceaeeb08b91e79fd7",
+    "url": "/static/js/main.0b764271.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
